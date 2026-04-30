c396fadec490a50f77f00590d01786b557218c35
haonan chen <haonanc123@gmail.com>
2026-04-30 15:43:07 -0700

    win sound

--- sound.h
+++ sound.h
@@ -11,6 +11,8 @@ ALuint bufMenu;
 ALuint srcMenu;
 ALuint bufNotime;
 ALuint srcNotime;
+ALuint bufWin;
+ALuint srcWin;
 
 inline void sound_init() {
     alutInit(0, NULL);
@@ -23,18 +25,22 @@ inline void sound_init() {
     bufMenu  = alutCreateBufferFromFile("./menu_sound.wav");
     bufGame  = alutCreateBufferFromFile("./menu_sound.wav");
     bufNotime = alutCreateBufferFromFile("./no_time.wav");
+    bufWin = alutCreateBufferFromFile("./win_sound.wav");
     alGenSources(1, &srcIntro);
     alGenSources(1, &srcMenu);
     alGenSources(1, &srcGame);
     alGenSources(1, &srcNotime);
+    alGenSources(1, &srcWin);
     alSourcei(srcIntro, AL_BUFFER, bufIntro);
     alSourcei(srcMenu,  AL_BUFFER, bufMenu);
     alSourcei(srcGame,  AL_BUFFER, bufGame);
     alSourcei(srcNotime, AL_BUFFER, bufNotime);
+    alSourcei(srcWin, AL_BUFFER, bufWin);
     alSourcef(srcIntro, AL_GAIN, 1.0f); alSourcef(srcIntro, AL_PITCH, 1.0f);
     alSourcef(srcMenu,  AL_GAIN, 1.0f); alSourcef(srcMenu,  AL_PITCH, 1.0f);
     alSourcef(srcGame,  AL_GAIN, 1.0f); alSourcef(srcGame,  AL_PITCH, 1.0f);
     alSourcef(srcNotime, AL_GAIN, 1.0f); alSourcef(srcNotime, AL_PITCH, 0.4f);
+    alSourcef(srcWin, AL_GAIN, 1.0f); alSourcef(srcWin, AL_PITCH, 1.0f);
 }
 
 inline void sound_shutdown() {
@@ -42,10 +48,12 @@ inline void sound_shutdown() {
     alDeleteSources(1, &srcMenu);
     alDeleteSources(1, &srcGame);
     alDeleteSources(1, &srcNotime);
+    alDeleteSources(1, &srcWin);
     alDeleteBuffers(1, &bufIntro);
     alDeleteBuffers(1, &bufMenu);
     alDeleteBuffers(1, &bufGame);
     alDeleteBuffers(1, &bufNotime);
+    alDeleteBuffers(1, &bufWin);
     ALCcontext *ctx = alcGetCurrentContext();
     ALCdevice  *dev = alcGetContextsDevice(ctx);
     alcMakeContextCurrent(NULL);
